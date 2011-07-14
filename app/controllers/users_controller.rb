0c234c398d4f76bc41a01b538670c70d12739017
Joraaver Chahal <joraaverchahal@gmail.com>
2011-07-13 19:34:22 -0700

    With correct buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  def login
-  end
+  
   def logout
   end
   def create
     @user = User.new(params[:user])
     if @user.save
       flash[:success] = "Welcome to the SVL!"	    
-      redirect_to @user
+      render :action => "new"
     else
+      render "new"
     end
   end
   def new
